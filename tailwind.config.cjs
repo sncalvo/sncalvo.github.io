7c0be47c56c1884230e9752996eb0c84ad72e4e8
Santiago Calvo <sncalvo@hotmail.com>
2021-12-16 00:20:13 -0300

    Add fonts and skills

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,4 +2,11 @@ module.exports = {
   mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
   // more options here
+  theme: {
+    extend: {
+      backgroundImage: {
+        presentation: "url(/assets/test-background.png)",
+      },
+    },
+  },
 };
