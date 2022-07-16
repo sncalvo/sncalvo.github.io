860ca27c02a54ba324dd6f84d158667037092179
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-16 11:26:58 -0300

    Improve color scheme and increase astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
+import solid from '@astrojs/solid-js';
+
 export default {
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  buildOptions: {
-    site: "https://sncalvo.github.io",
-    sitemap: true, // Generate sitemap (set to "false" to disable)
-  },
-  devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
-  },
-  renderers: ["@astrojs/renderer-solid"],
+  site: 'https://sncalvo.github.io',
+  // sitemap: true, // Generate sitemap (set to "false" to disable)
+  // devOptions: {
+  // hostname: 'localhost',  // The hostname to run the dev server on.
+  // port: 3000,             // The port to run the dev server on.
+  // },
+  integrations: [solid()],
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,53 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
+  mode: 'jit',
+  content: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
   // more options here
   theme: {
     extend: {
       backgroundImage: {
-        presentation: "url(/assets/test-background.png)",
+        presentation: 'url(/assets/test-background.png)',
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#0071CD',
+          100: '#0071CD',
+          200: '#005092',
+          300: '#003561',
+          400: '#002039',
+          500: '#011627',
+        },
+        secondary: {
+          DEFAULT: '#FFF0DC',
+          100: '#FFF0DC',
+          200: '#FFE4BE',
+          300: '#FFCF8E',
+          400: '#FFB754',
+          500: '#FF9F1C',
+        },
+        tertiary: {
+          DEFAULT: '#41EAD4',
+          100: '#41EAD4',
+          200: '#2EBAA8',
+          300: '#288C7F',
+          400: '#185A52',
+          500: '#0D3A34',
+        },
+        danger: {
+          DEFAULT: '#E9C5CA',
+          100: '#E9C5CA',
+          200: '#E29C9E',
+          300: '#E38591',
+          400: '#E5495E',
+          500: '#F71735',
+        },
+        white: {
+          DEFAULT: '#FDFFFC',
+          100: '#FDFFFC',
+          200: '#CACBC9',
+          300: '#9B9B9B',
+          400: '#6F6F6F',
+          500: '#434343',
+        },
       },
     },
   },
