6be2155e05d4475388c0cd90306279cd52e0a8c6
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-16 14:35:55 -0300

    Add contact section

---
+++ src/components/molecules/Contact.jsx
@@ -0,0 +1,61 @@
+import { createSignal } from 'solid-js';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  message: yup.string().required(),
+});
+
+export default Contact = () => {
+  const [emailInfo, setEmailInfo] = createSignal({ email: '', message: '' });
+  const [error, setError] = createSignal('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await schema.validate(emailInfo);
+      setError('');
+      setEmailInfo({ email: '', message: '' });
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div className="flex flex-col" id="Contact">
+      <h1 className="text-3xl text-secondary-500">Contact:</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col space-y-2 p-2 border-2 rounded border-secondary-500 relative">
+          <label htmlFor="email" className="text-secondary-500">
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            value={emailInfo().email}
+            onChange={(e) => setEmailInfo((prevState) => ({ ...prevState, email: e.target.value }))}
+            className="w-full p-2 border-2 rounded border-secondary-500 text-secondary-500 bg-transparent"
+          />
+          <label htmlFor="message" className="text-secondary-500">
+            Message
+          </label>
+          <textarea
+            id="message"
+            value={emailInfo().message}
+            onChange={(e) =>
+              setEmailInfo((prevState) => ({ ...prevState, message: e.target.value }))
+            }
+            className="w-full p-2 border-2 rounded border-secondary-500 text-secondary-500 bg-transparent"
+          />
+          <button
+            type="submit"
+            className="w-full p-2 border-2 text-secondary-500 rounded border-secondary-500"
+          >
+            Send
+          </button>
+        </div>
+      </form>
+      {error && <p className="text-red-500">{error}</p>}
+    </div>
+  );
+};
