57842b663baa09724a5a5074e49ca3f2ba6411c6
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-17 00:27:40 -0300

    Add programmer icon

--- src/components/molecules/Menu.jsx
+++ src/components/molecules/Menu.jsx
@@ -51,8 +51,8 @@ export default function SolidCounter() {
             '-translate-x-full': !open(),
           }}
         >
-          <div class="absolute left-2 top-2 bottom-0 right-0 bg-primary-500 blur-sm z-0" />
-          <ul class="flex flex-col space-y-2 p-2 border-2 rounded border-secondary-500 relative">
+          <div class="absolute left-2 top-2 bottom-0 right-0 bg-primary-500 blur-sm z-10" />
+          <ul class="flex flex-col space-y-2 p-2 border-2 rounded border-secondary-500 z-20 relative">
             <PageLink name="Presentation" id="Presentation" />
             <PageLink name="Skill Set" id="SkillSet" />
             <PageLink name="About" id="About" />
