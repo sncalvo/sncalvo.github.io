acd8cb3144f13a78eaf8a52a29ace65735f43dc5
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-08-29 21:00:45 -0300

    Add sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,29 +2,11 @@ import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import vercel from '@astrojs/vercel/serverless';
 
-// export default {
-//   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-//   // pages: './src/pages', // Path to Astro components, pages, and data
-//   // dist: './dist',       // When running `astro build`, path to final static output
-//   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-//   site: 'https://sncalvo.github.io',
-//   // sitemap: true, // Generate sitemap (set to "false" to disable)
-//   // devOptions: {
-//   // hostname: 'localhost',  // The hostname to run the dev server on.
-//   // port: 3000,             // The port to run the dev server on.
-//   // },
-//   integrations: [solid()],
-//   vite: {
-//     ssr: {
-//       external: ['svgo'],
-//     },
-//   },
-//   adapter: vercel(),
-// };
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://sncalvo.github.io',
-  integrations: [solid()],
+  site: 'https://scalvo.dev',
+  integrations: [solid(), sitemap()],
   vite: {
     ssr: {
       external: ['svgo'],
