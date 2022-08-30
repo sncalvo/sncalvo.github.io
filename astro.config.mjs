3cb4ccad4d4f036358928a4e2eb9b9e15e51de7b
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-08-29 21:23:45 -0300

    Add partytown with google tag manager

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import vercel from '@astrojs/vercel/serverless';
-
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://scalvo.dev',
   integrations: [
     solid(),
     sitemap({
       customPages: ['https://scalvo.dev', 'https://scalvo.dev/blog'],
+    }),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
     }),
   ],
   vite: {
