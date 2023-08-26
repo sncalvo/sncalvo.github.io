0ecc0534ef075e8ff1498fe95948f5bec4c72101
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2023-08-26 11:53:47 -0300

    Fix: Use sqlite web client

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@libsql/client";
+import { createClient } from "@libsql/client/http";
 import { drizzle } from 'drizzle-orm/libsql';
 
 const client = createClient({ url: import.meta.env.DATABASE_URL, authToken: import.meta.env.DATABASE_AUTH_TOKEN });
