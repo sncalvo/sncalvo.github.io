d0a746cbc16211cfec8f02b3bf05989ddb03d593
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2023-09-03 17:39:50 -0300

    Use Deno.env env variables

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@libsql/client/web";
 import { drizzle } from 'drizzle-orm/libsql';
 
+const DATABASE_URL = import.meta.env.DATABASE_URL || Deno.env.get('DATABASE_URL');
+const DATABASE_AUTH_TOKEN = import.meta.env.DATABASE_AUTH_TOKEN || Deno.env.get('DATABASE_AUTH_TOKEN');
+
 const client = createClient({ url: import.meta.env.DATABASE_URL, authToken: import.meta.env.DATABASE_AUTH_TOKEN });
 
 export const db = drizzle(client);
